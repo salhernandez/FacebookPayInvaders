c6788090f5d5f74f8253147f4dd3ce5e1641bdc7
Salvador Hernandez <salvhernandez@csumb.edu>
2017-03-07 05:41:31 +0000

    leap of faith

--- app.py
+++ app.py
@@ -47,17 +47,12 @@ def getNameOfUser(anID):
 
 @app.route('/', methods=['GET'])
 def verify():
-    
-    # models.db.session.add()
-    # models.db.session.commit()
-        
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
-    
     return "Hello world", 200
 
 @app.route('/', methods=['POST'])
