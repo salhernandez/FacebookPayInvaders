44ffdf413ae6e2bb944439c2c26b075c6f6cc38c
pomelovanna2 <annapomelov@outlook.com>
2017-03-25 04:33:13 +0000

    testing

--- app.py
+++ app.py
@@ -115,24 +115,24 @@ def hello():
     
     return render_template('index.html', user_info = df3.to_html(), pay = df.to_html(), payed = df2.to_html(), owed = g3.to_html(), friends = message4)
 
-# def getIDofUser(someText):
-#     #convert string to all lower case for easier processing 
-#     someText = someText.lower()
-#     usrID = False
-#     userFirst = ""
-#     if 'pay josh' or 'make payment to josh' in someText:
-#         usrID = str(985245348244242)
-#         userFirst = "josh"
+def getIDofUser(someText):
+    #convert string to all lower case for easier processing 
+    someText = someText.lower()
+    usrID = False
+    userFirst = ""
+    if 'pay josh' or 'make payment to josh' in someText:
+        usrID = str(985245348244242)
+        userFirst = "josh"
     
-#     elif 'sal' or 'pay sal' or 'make payment to sal' in someText:
-#         usrID = str(1596606567017003)
-#         userFirst = "sal"
+    elif 'sal' or 'pay sal' or 'make payment to sal' in someText:
+        usrID = str(1596606567017003)
+        userFirst = "sal"
     
-#     elif 'anna' or 'pay anna' or 'make payment to anna' in someText:
-#         usrID = str(1204927079622878)
-#         userFirst = "anna"
+    elif 'anna' or 'pay anna' or 'make payment to anna' in someText:
+        usrID = str(1204927079622878)
+        userFirst = "anna"
     
-#     return usrID, userFirst
+    return usrID, userFirst
 
 
 def getNameOfUser(anID):
