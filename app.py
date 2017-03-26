10a109bb22fd6c3d13eadb7443b6d1e44d2c7d66
Josh Smith <josmith@csumb.edu>
2017-03-26 23:08:11 +0000

    testing form

--- app.py
+++ app.py
@@ -26,16 +26,16 @@ db = flask_sqlalchemy.SQLAlchemy(app)
 SENTINEL = "-1"
 SENTINEL_FLOAT = -1.0
 
-@app.route('/data', methods=['GET'])
+@app.route('/data', methods=['POST'])
 def hello():
     
     # give ID's a name
     names = {985245348244242: "Josh", 1596606567017003: "Sal", 1204927079622878: "Anna"}
     
-    payedUser = UserInfo.UserInfo(names[int(request.form['pid'])], str(request.form['pid']))
-    senderUser = UserInfo.UserInfo(names[int(request.form['fid'])], str(request.form['fid']))
-    sendMsg = MsgBuilder.MessageBuilder(fromUser = senderUser, toUser = payedUser, messageType="simple", amount = str(request.form['amount']))
-    sendMsg.notify_payee_and_payer_of_payment()
+    # payedUser = UserInfo.UserInfo(names[int(request.form['pid'])], str(request.form['pid']))
+    # senderUser = UserInfo.UserInfo(names[int(request.form['fid'])], str(request.form['fid']))
+    # sendMsg = MsgBuilder.MessageBuilder(fromUser = senderUser, toUser = payedUser, messageType="simple", amount = str(request.form['amount']))
+    # sendMsg.notify_payee_and_payer_of_payment()
     # used to insert values into database
     ########################################################################
     # # Pay
