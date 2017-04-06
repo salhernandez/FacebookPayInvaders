b64e13a443794921cdc8b1511bb91f8bc52da297
Josh Smith <josmith@csumb.edu>
2017-04-06 23:14:11 +0000

    testing pay gateway

--- classes/Pay.py
+++ classes/Pay.py
@@ -48,54 +48,12 @@ class PayGate(object):
 
         # convert dict into json
         #####################################
-        JSON_Datalist = """{
-             attachment: {
-                "type": "template",
-                "payload": {
-                  "template_type": "generic",
-                  "elements": [{
-                    "title": "rift",
-                    "subtitle": "Next-generation virtual reality",
-                    "item_url": "https://www.oculus.com/en-us/rift/",               
-                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                    "buttons": [{
-                      "type": "web_url",
-                      "url": "https://www.oculus.com/en-us/rift/",
-                      "title": "Open Web URL"
-                    }, {
-                      "type": "postback",
-                      "title": "Call Postback",
-                      "payload": "Payload for first bubble",
-                    }],
-                  }, {
-                    "title": "touch",
-                    "subtitle": "Your Hands, Now in VR",
-                    "item_url": "https://www.oculus.com/en-us/touch/",               
-                    "image_url": "http://messengerdemo.parseapp.com/img/touch.png",
-                    "buttons": [{
-                      "type": "web_url",
-                      "url": "https://www.oculus.com/en-us/touch/",
-                      "title": "Open Web URL"
-                    }, {
-                      "type": "postback",
-                      "title": "Call Postback",
-                      "payload:" "Payload for second bubble",
-                    }]
-                  }]
-                }
-              }
-            }
-        }""" 
-
-        dataDict = {}
-        dataDict['recipient'] = {}
-        dataDict['message'] = {}
-
+        JSON_Datalist = """{"recipient":{"id":"recipientId"},"message":{"attachment":{"type":"template","payload":{"template_type":"generic","elements":[{"title":"rift","subtitle":"Next-generation virtual reality","item_url":"https://www.oculus.com/en-us/rift/","image_url":"http://messengerdemo.parseapp.com/img/rift.png","buttons":[{"type":"web_url","url":"https://www.oculus.com/en-us/rift/","title":"Open Web URL"},{"type":"postback","title":"Call Postback","payload":"Payload for first bubble"}]},{"title":"touch","subtitle":"Your Hands, Now in VR","item_url":"https://www.oculus.com/en-us/touch/","image_url":"http://messengerdemo.parseapp.com/img/touch.png","buttons":[{"type":"web_url","url":"https://www.oculus.com/en-us/touch/","title":"Open Web URL"},{"type":"postback","title":"Call Postback","payload":"Payload for second bubble"}]}]}}}}""" 
+    
         the_dict = json.loads(JSON_Datalist)
-        dataDict['recipient']['id'] = str(toID)
-        dataDict['message'] = the_dict
+        the_dict['recipient']['id'] = str(toID)
 
-        data = json.dumps(dataDict)
+        data = json.dumps(the_dict)
         #######################################
         r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
         if r.status_code != 200:
