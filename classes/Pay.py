c57aee3fe39290c5d2f8baea033f6d3ea2b69c1d
Josh Smith <josmith@csumb.edu>
2017-04-07 20:15:38 +0000

    new table for friends

--- classes/Pay.py
+++ classes/Pay.py
@@ -59,12 +59,47 @@ class PayGate(object):
         if r.status_code != 200:
             self.log(r.status_code)
             self.log(r.text)
+            
+    def get_user_template_simple(self, toID):
+
+        params = {
+            "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+        }
+        headers = {
+            "Content-Type": "application/json"
+        }
+
+        # data = json.dumps({
+        #     "recipient": {
+        #         "id": recipient_id
+        #     },
+        #     "message": {
+        #         "text": message_text
+        #     }
+        # })
+
+        # convert dict into json
+        #####################################
+        JSON_Datalist = """{"recipient":{"id":"RECIPIENT_ID"},"message":{"attachment":{"type":"template","payload":{"template_type":"list","elements":[{"title":"Classic T-Shirt Collection","image_url":"https://peterssendreceiveapp.ngrok.io/img/collection.png","subtitle":"See all our colors","default_action":{"type":"web_url","url":"https://peterssendreceiveapp.ngrok.io/shop_collection","messenger_extensions":true,"webview_height_ratio":"tall","fallback_url":"https://peterssendreceiveapp.ngrok.io/"},"buttons":[{"title":"View","type":"web_url","url":"https://peterssendreceiveapp.ngrok.io/collection","messenger_extensions":true,"webview_height_ratio":"tall","fallback_url":"https://peterssendreceiveapp.ngrok.io/"}]},{"title":"Classic White T-Shirt","image_url":"https://peterssendreceiveapp.ngrok.io/img/white-t-shirt.png","subtitle":"100% Cotton, 200% Comfortable","default_action":{"type":"web_url","url":"https://peterssendreceiveapp.ngrok.io/view?item=100","messenger_extensions":true,"webview_height_ratio":"tall","fallback_url":"https://peterssendreceiveapp.ngrok.io/"},"buttons":[{"title":"Shop Now","type":"web_url","url":"https://peterssendreceiveapp.ngrok.io/shop?item=100","messenger_extensions":true,"webview_height_ratio":"tall","fallback_url":"https://peterssendreceiveapp.ngrok.io/"}]},{"title":"Classic Blue T-Shirt","image_url":"https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png","subtitle":"100% Cotton, 200% Comfortable","default_action":{"type":"web_url","url":"https://peterssendreceiveapp.ngrok.io/view?item=101","messenger_extensions":true,"webview_height_ratio":"tall","fallback_url":"https://peterssendreceiveapp.ngrok.io/"},"buttons":[{"title":"Shop Now","type":"web_url","url":"https://peterssendreceiveapp.ngrok.io/shop?item=101","messenger_extensions":true,"webview_height_ratio":"tall","fallback_url":"https://peterssendreceiveapp.ngrok.io/"}]},{"title":"Classic Black T-Shirt","image_url":"https://peterssendreceiveapp.ngrok.io/img/black-t-shirt.png","subtitle":"100% Cotton, 200% Comfortable","default_action":{"type":"web_url","url":"https://peterssendreceiveapp.ngrok.io/view?item=102","messenger_extensions":true,"webview_height_ratio":"tall","fallback_url":"https://peterssendreceiveapp.ngrok.io/"},"buttons":[{"title":"Shop Now","type":"web_url","url":"https://peterssendreceiveapp.ngrok.io/shop?item=102","messenger_extensions":true,"webview_height_ratio":"tall","fallback_url":"https://peterssendreceiveapp.ngrok.io/"}]}],"buttons":[{"title":"View More","type":"postback","payload":"payload"}]}}}}""" 
+    
+        the_dict = json.loads(JSON_Datalist)
+        the_dict['recipient']['id'] = str(toID)
+
+        data = json.dumps(the_dict)
+        #######################################
+        r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+        if r.status_code != 200:
+            self.log(r.status_code)
+            self.log(r.text)
     
     ##send message to user 
     ############################################################################
 
     def send_payment_gateway(self):
         self.message_template_simple(self.toID)
+        
+    def send_user_table(self):
+        self.get_user_template_simple(self.toID)
 
     ############################################################################
     def log(self, text):  # simple wrapper for __log__ging to stdout on heroku
