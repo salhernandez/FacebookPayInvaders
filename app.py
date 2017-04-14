c624bea07f09d49521341a96a4ae070e9f3bd91b
Salvador Hernandez <salvhernandez@csumb.edu>
2017-04-14 07:15:04 +0000

    testing...

--- app.py
+++ app.py
@@ -265,7 +265,7 @@ def webhook():
                         # flowType = messaging_event['message']['quick_reply']['payload']['flowType']
                         # value = messaging_event['message']['quick_reply']['payload']['value']
                         
-                        for info in messaging_event['message']['quick_reply']['payload']:
+                        for info in messaging_event['message']['quick_reply']:
                             log(info)
                             log(info['flowType'])
                             log(info['value'])
