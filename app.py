acf37a5e4416dc610d49308857b12dab5b27fcc4
Josh Smith <josmith@csumb.edu>
2017-04-14 23:23:37 +0000

    test

--- app.py
+++ app.py
@@ -398,10 +398,10 @@ def webhook():
                                 
                             
                             #sends buttons with images to josh
-                            if "josh button demo" in message_text:
+                            if "Josh button demo" in message_text:
                                 the_payment.send_user_table()
                                 dbLink = DBLink.DBLink()
-                                the_user = dbLink.get_users_with_first_name("Hsoj")
+                                the_user = dbLink.get_users_with_first_name("Tala")
                                 print the_user
                                 break
                             
