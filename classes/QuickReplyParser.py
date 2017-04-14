c108e1608f1d686f48aa22295d84c697e22d39d4
Salvador Hernandez <salvhernandez@csumb.edu>
2017-04-14 16:56:33 +0000

    added first flowtype checks

--- classes/QuickReplyParser.py
+++ classes/QuickReplyParser.py
@@ -1,4 +1,4 @@
-import requests, os, json
+import requests, os, json, sys
 
 # This class is meant to containt all the graph request that we will use
 
@@ -10,9 +10,41 @@ class QuickReplyParser(object):
         self.flowType = str(flowType).lower()
         self.flowState = flowState
         self.senderID = senderID
+        
+        
+        if self.flowType is "pay":
+            self.__payFlow__()
+        elif self.flowType is "request":
+            self.__requestFlow__()
+        elif self.flowType is "split":
+            self.__splitFlow__()
+        else:
+            return None
+        
+    """
+    payFlow
+    """
+    def __payFlow__(self):
+        self.log("its the pay flow :)")
+    
+    """
+    requestFLow
+    """
+    def __requestFlow__(self):
+        self.log("its the request flow :)")
+    
+    """
+    splitFLow
+    """
+    def __splitFlow__(self):
+        self.log("its the split flow :)")
     
     """
     prints the instance variables as a dictionary
     """
     def __str__(self):
         return str(self.__dict__)
+    
+    def log(self, message):  # simple wrapper for logging to stdout on heroku
+        print str(message)
+        sys.stdout.flush()
