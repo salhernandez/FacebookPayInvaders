974c999ad39eccc5b02d112dc226b0594753f8c4
Salvador Hernandez <salvhernandez@csumb.edu>
2017-04-14 04:10:29 +0000

    added try except to code block

--- app.py
+++ app.py
@@ -254,52 +254,51 @@ def webhook():
                         log("QUICK REPLY ERROR CHECK")
                         log("flowType: "+messaging_event["message"]["quick_reply"]['flowType'])
                         log("value: "+messaging_event["message"]["quick_reply"]['value'])
-                    except KeyError:
-                        break
-                            
-                    #Check where sender is in flow
-                    dbLink = DBLink.DBLink()
-                    flow_info = dbLink.get_flow_state(sender_id)
+                        #Check where sender is in flow
+                        dbLink = DBLink.DBLink()
+                        flow_info = dbLink.get_flow_state(sender_id)
+                        
+                        sendMsg = MsgBuilder.MessageBuilder(fromUser = sender_id)
+                        
+                        flow_type = flow_info['flowType']
+                        flow_state = flow_info['flowState']
                     
-                    
-                    flow_type = flow_info['flowType']
-                    flow_state = flow_info['flowState']
-                
-                    #for testing values of flowstate and flowtype
-                    
-                    log("FLOWSTATE")
-                    log(flow_info['flowState'])
-                    log("FLOWTYPE")
-                    log(flow_info['flowType'])
-                    
-                    #if state is 0 we want to send the default buttons no matter what
-                    if flow_state == 0:
-                        #send pay, request, split quick reply
-                        sendMsg.send_default_message()
-                        break
-                    
-                    #if state not 0 we need to parse the message further
-                    elif flow_state != 0:  
-                        #check if the user used a quick_reply
-                        try:
-                            log("QUICK REPLY")
-                            quick_reply = messaging_event["message"]["quick_reply"]
-                            #log("THIS IS WHAT WE GET FOMR THE QUICK REPLY: "+quick_reply)
-                            log("flowType: "+messaging_event["message"]["quick_reply"]['flowType'])
-                            log("value: "+messaging_event["message"]["quick_reply"]['value'])
+                        #for testing values of flowstate and flowtype
+                        
+                        log("FLOWSTATE")
+                        log(flow_info['flowState'])
+                        log("FLOWTYPE")
+                        log(flow_info['flowType'])
+                        
+                        #if state is 0 we want to send the default buttons no matter what
+                        if flow_state == 0:
+                            #send pay, request, split quick reply
+                            sendMsg.send_default_message()
                             break
-                            
-                        except KeyError:
-                            log("QUICKREPLY NOT FOUND")
-                            if flow_state == 2:
-                                the_payment = PayGate(toUser = messaging_event["sender"]["id"])
-                                the_payment.send_user_table()
+                        
+                        #if state not 0 we need to parse the message further
+                        elif flow_state != 0:  
+                            #check if the user used a quick_reply
+                            try:
+                                log("QUICK REPLY")
+                                quick_reply = messaging_event["message"]["quick_reply"]
+                                #log("THIS IS WHAT WE GET FOMR THE QUICK REPLY: "+quick_reply)
+                                log("flowType: "+messaging_event["message"]["quick_reply"]['flowType'])
+                                log("value: "+messaging_event["message"]["quick_reply"]['value'])
                                 break
-                            elif flow_state == 4:
-                                sendMsg.send_confirmation_message()
-                                break
-                    
-
+                                
+                            except KeyError:
+                                log("QUICKREPLY NOT FOUND")
+                                if flow_state == 2:
+                                    the_payment = PayGate(toUser = messaging_event["sender"]["id"])
+                                    the_payment.send_user_table()
+                                    break
+                                elif flow_state == 4:
+                                    sendMsg.send_confirmation_message()
+                                    break
+                    except KeyError:
+                        pass
+                        
 ################################################################################################### 
                 
                     # the message's text
