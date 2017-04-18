12dd20e7d149450b9241b1d037660002d26d7322
Josh Smith <josmith@csumb.edu>
2017-04-18 01:59:50 +0000

    button fix

--- classes/Pay.py
+++ classes/Pay.py
@@ -93,8 +93,8 @@ class PayGate(object):
         for i in range(len(users)-1):
             the_dict['message']['quick_replies'][i]['title'] = users[i]['firstName']
             the_dict['message']['quick_replies'][i]['image_url'] = users[i]['imgUrl']
-            # the_dict['message']['quick_replies'][i]['payload'] = str(users[i]['userID'])
-            print the_dict['message']['quick_replies'][i]['payload']
+            the_dict['message']['quick_replies'][i]['payload'] = str(users[i]['userID'])
+            print the_dict['message']['quick_replies'][i]['payload'] = 
 
         data = json.dumps(the_dict)
         #######################################
