2a5510e286ab318a4b31bcc9813325e3f1f8c1cd
Josh Smith <josmith@csumb.edu>
2017-04-28 00:08:12 +0000

    testing

--- app.py
+++ app.py
@@ -433,10 +433,11 @@ def webhook():
                                 people = []
                                 p_count = 0;
                                 for a_result in result1:
-                                    print a_result
-                                    # got_user = dbLink.get_user_in_db(str(a_result['owed_ID']))
-                                    # people[p_count] = got_user['firstName'] + " " + got_user['lastName'] + " " + str(a_result['owed_ID']['amount'])
-                                    # p_count = p_count + 1
+                                    
+                                    got_user = dbLink.get_user_in_db(str(a_result['owed_ID']))
+                                    print got_user['firstName']
+                                    people[p_count] = got_user['firstName'] + " " + got_user['lastName'] + " " + str(a_result['owed_ID']['amount'])
+                                    p_count = p_count + 1
                                 
                                 
                                 
